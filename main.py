c5989eb7354763d5da36c5bf376cd09bf655867d
Satenik <satenik_tovmasyan@edu.aua.am>
2018-11-29 00:46:17 +0400

    Basics added

---
+++ main.py
@@ -0,0 +1,27 @@
+class Cards_Set:
+    def __init__(self, head):
+        self._head = head
+
+    def sethead(self):
+        return self._head
+
+    def gethead(self):
+        return self._head
+
+class Card:
+    def __init__(self, word, definition):
+        self.word = word
+        self.definition = definition
+        self.next = None
+
+
+
+
+
+
+def main():
+
+
+
+
+main()
