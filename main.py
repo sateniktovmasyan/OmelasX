29c6c8b78972e8bee5bfc0289266a38da102c14e
Satenik <satenik_tovmasyan@edu.aua.am>
2018-11-29 00:59:12 +0400

    class format changed

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+class Card:
+    def __init__(self, word, definition):
+        self.word = word
+        self.definition = definition
+        self.next = None
+
 class Cards_Set:
     def __init__(self, head):
         self._head = head
@@ -8,20 +14,32 @@ class Cards_Set:
     def gethead(self):
         return self._head
 
-class Card:
-    def __init__(self, word, definition):
-        self.word = word
-        self.definition = definition
-        self.next = None
+    def append(self, data, data2):
+        new_node = Card(data == raw_input(), data2 == raw_input())
 
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
 
 
 
 
 
 def main():
-
-
+    ca = Cards_Set("")
+    ca.append(raw_input("Please input a word "), raw_input("Please input a definition "))
+    ca.print_list()
 
 
 main()
